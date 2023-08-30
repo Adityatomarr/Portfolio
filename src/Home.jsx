9c3ed6608092feb622c4d505f31255e321b4b393
Aditya <aditya.tomar.115@gmail.com>
2023-08-30 23:16:42 +0530

    add responsive typography

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,7 @@ export default function Home(){
         <div>
             <div className="grid-container">
                 <div>
-                    <h3 className="ff-sans-normal fs-900">Hi, I am <span className=" text-accent "> Aditya Tomar,</span> </h3>
+                    <h3 className="ff-sans-normal fs-900">Hi, I am <span className=" text-accent d-block "> Aditya Tomar,</span> </h3>
                     <h2 className="ff-sans-normal fs-800">Web developer and web designer</h2>
                     <p className="fs-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae nihil velit iste consequuntur, veniam repellat ratione cupiditate minima quas ab adipisci esse iusto maiores modi, quia omnis illum eius aliquam.</p>
                 </div>
