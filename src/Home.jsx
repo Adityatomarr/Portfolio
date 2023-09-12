139ca699802e674002dec31c79ba9389bee006a3
Aditya <aditya.tomar.115@gmail.com>
2023-09-12 18:03:57 +0530

    fix mobile background image

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,11 +11,12 @@ export default function Home(){
                 <div>
                     <p className="ff-sans-normal fs-800">Hi, I am</p>
                     <h3 className="ff-sans-normal fs-900 text-accent d-block ">Aditya Tomar</h3>
-                    <h2 className="ff-sans-normal fs-800">Web developer and web designer</h2>
+                    <h2 className="ff-sans-normal fs-800">Web developer and Web designer</h2>
                     {/* <p className="fs-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae nihil velit iste consequuntur, veniam repellat ratione cupiditate minima quas ab adipisci esse iusto maiores modi, quia omnis illum eius aliquam.</p> */}
+                    <Link to="projects" className="large-button uppercase ff-serif text-white">About &rarr;</Link>
                 </div>
                 <div>
-                    {/* <Link to="projects" className="large-button uppercase ff-serif text-dark bg-white">About</Link> */}
+                    
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import "./navbar.css"
 
 const Navbar = () => {
   return (
-    <nav>
-      {/* <ul className="primary-navigation flex underline-indicators">
-        <li className='text-white fs-500 ff-sans-cond letter-spacing-3'>
-          <NavLink to="/" className={({isActive})=> isActive? "active" : "" } ><span>00</span>Home</NavLink>
-        </li>
-        <li className='text-white fs-500 ff-sans-cond letter-spacing-2'>
-          <NavLink to="about" className={({isActive})=> isActive? "active" : "" } ><span>01</span>About</NavLink>
-        </li>
-        <li className='text-white fs-500 ff-sans-cond letter-spacing-2'>
-          <NavLink to="/projects" className={({isActive})=> isActive? "active" : "" } ><span>02</span>Projects</NavLink>
-        </li>
-        <li className='text-white fs-500 ff-sans-cond letter-spacing-2'>
-          <NavLink to="/contact" className={({isActive})=> isActive? "active" : "" } ><span>03</span>Contact</NavLink>
-        </li>
-      </ul> */}
-    </nav>
+    <div>
+      <button className='mobile-nav-toggle' aria-controls='primary-navigation' aria-expanded='false '><span className='sr-only'>Menu</span></button>
+      <nav>
+        <ul id="primary-navigation" className="primary-navigation flex underline-indicators">
+          <li className='text-white fs-500 ff-sans-cond letter-spacing-3'>
+            <NavLink to="/" className={({isActive})=> isActive? "active" : "" } ><span>00</span>Home</NavLink>
+          </li>
+          <li className='text-white fs-500 ff-sans-cond letter-spacing-2'>
+            <NavLink to="about" className={({isActive})=> isActive? "active" : "" } ><span>01</span>About</NavLink>
+          </li>
+          <li className='text-white fs-500 ff-sans-cond letter-spacing-2'>
+            <NavLink to="/projects" className={({isActive})=> isActive? "active" : "" } ><span>02</span>Projects</NavLink>
+          </li>
+          <li className='text-white fs-500 ff-sans-cond letter-spacing-2'>
+            <NavLink to="/contact" className={({isActive})=> isActive? "active" : "" } ><span>03</span>Contact</NavLink>
+          </li>
+      </ul>
+      </nav>
+      
+    </div>
+    
   )
 }
 
