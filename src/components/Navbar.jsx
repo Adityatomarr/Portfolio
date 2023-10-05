c3530a586b7ba1d44e233a60cf7eab6997cb92c9
Aditya <aditya.tomar.115@gmail.com>
2023-10-05 23:52:00 +0530

    add animation to mobile navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import {React , useState} from 'react'
 import { NavLink } from 'react-router-dom'
 import "./navbar.css"
+import logo from "../assets/logo.png"
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>
-      <button className='mobile-nav-toggle' aria-controls='primary-navigation' aria-expanded='false '><span className='sr-only'>Menu</span></button>
+    <div className='primary-header flex' >
+      <img width={"60px"} height={"60px"}src={logo}></img>
+      <button className= {toggle ? 'mobile-nav-toggle expanded ': 'mobile-nav-toggle'}
+        onClick={()=>setToggle(!toggle)}
+        aria-controls='primary-navigation' 
+        aria-expanded='false '><span className='sr-only'>Menu</span>
+      </button>
       <nav>
-        <ul id="primary-navigation" className="primary-navigation flex underline-indicators">
+        <ul id="primary-navigation" 
+        className={toggle? "primary-navigation flex underline-indicators expanded": "primary-navigation flex underline-indicators"}>
           <li className='text-white fs-500 ff-sans-cond letter-spacing-3'>
             <NavLink to="/" className={({isActive})=> isActive? "active" : "" } ><span>00</span>Home</NavLink>
           </li>
